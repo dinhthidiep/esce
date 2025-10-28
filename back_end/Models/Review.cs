2a41a25ac69edf46ae8f6b162e3247fe673ad78b
Epxingtu <diepdtde170087@fpt.edu.vn>
2025-10-28 11:21:35 +0700

    chá»©c nÄƒng booking vÃ  review nhÃ©

--- back_end/Models/Review.cs
+++ back_end/Models/Review.cs
@@ -1,26 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ESCE_SYSTEM.Models
+namespace Learnasp.Models
 {
-    public partial class Review
+    public class Review
     {
-        public Review()
-        {
-            InverseParentReview = new HashSet<Review>();
-        }
-
+        [Key]
         public int Id { get; set; }
-        public int TourId { get; set; }
-        public int AuthorId { get; set; }
-        public int? ParentReviewId { get; set; }
-        public int? Rating { get; set; }
-        public string Content { get; set; } = null!;
-        public DateTime? CreatedAt { get; set; }
 
-        public virtual Account Author { get; set; } = null!;
-        public virtual Review? ParentReview { get; set; }
-        public virtual Tour Tour { get; set; } = null!;
-        public virtual ICollection<Review> InverseParentReview { get; set; }
+        [Required]
+        [Column("BookingId")]
+        public int BookingId { get; set; }
+
+        [Required]
+        [Column("UserId")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        [Column("Rating")]
+        public int Rating { get; set; } // 1-5 sao
+
+        [MaxLength(1000)]
+        [Column("Comment")]
+        public string? Comment { get; set; }
+
+        [Column("CreatedDate")]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        [MaxLength(50)]
+        [Column("Status")]
+        public string Status { get; set; } = "pending"; // pending / approved / rejected
+
+        // Navigation properties
+        [ForeignKey("BookingId")]
+        public virtual Booking Booking { get; set; } = null!;
+
+        [ForeignKey("UserId")]
+        public virtual Account User { get; set; } = null!;
     }
 }
+
+
